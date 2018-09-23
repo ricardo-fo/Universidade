8f40a5d6bf85de777e6c74f0ae3825ca7d7a93d3
Ricardo Oliveira <40704273+ricardo-fo@users.noreply.github.com>
2018-09-23 16:06:34 -0300

    ex-04-trabalho-finalizar[fix binary]

--- Primeiro ano/Algoritmos/ex-04entrega.c
+++ Primeiro ano/Algoritmos/ex-04entrega.c
@@ -41,30 +41,30 @@ int main()
 			cpyFileBin(pone, ptwo);
 			fclose(pone);
 			if((pone = fopen(file_two, "rb")) == NULL){
-                printf("Arquivo '%s' vazio ou inexistente!\nImpossivel prosseguir com a gravacao!\n", file_two);
-                return 2;
+                		printf("Arquivo '%s' vazio ou inexistente!\nImpossivel prosseguir com a gravacao!\n", file_two);
+                		return 2;
 			}
 			cpyFileBin(pone, ptwo);
 			fclose(pone);
 			fclose(ptwo);
 			}else{
-            	if((pone = fopen(file_one, "r")) == NULL){
-                	printf("Arquivo '%s' vazio ou inexistente!\nImpossivel fazer a gravacao!\n", file_one);
-                	return 2;
-           		}
-            	ptwo = fopen(file_three,"w");
+            			if((pone = fopen(file_one, "r")) == NULL){
+                			printf("Arquivo '%s' vazio ou inexistente!\nImpossivel fazer a gravacao!\n", file_one);
+                			return 2;
+           			}
+            			ptwo = fopen(file_three,"w");
 				cpyFile(pone, ptwo);
 				fclose(pone);
 				if((pone = fopen(file_two, "r")) == NULL){
-                	printf("Arquivo '%s' vazio ou inexistente!\nImpossivel prosseguir com a gravacao!\n", file_two);
-               		return 2;
+                			printf("Arquivo '%s' vazio ou inexistente!\nImpossivel prosseguir com a gravacao!\n", file_two);
+               				return 2;
 				}
 				cpyFile(pone, ptwo);
 				fclose(pone);
 				fclose(ptwo);
 			}
 	}
-    return 0;
+	return 0;
 }
 
 void getExt(const char * file, char * ext)
