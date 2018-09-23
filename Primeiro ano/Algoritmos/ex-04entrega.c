fe09b3fb6d439368fac3abae56b048efae133bee
Ricardo Oliveira <40704273+ricardo-fo@users.noreply.github.com>
2018-09-23 15:59:25 -0300

    ex-04-trabalho-finalizado

--- Primeiro ano/Algoritmos/ex-04entrega.c
+++ Primeiro ano/Algoritmos/ex-04entrega.c
@@ -1,4 +1,4 @@
-/* 
+/*
 Faça um programa que receba dois arquivos do usuário, e crie um terceiro
 arquivo com o conteúdo dos dois primeiros juntos (o conteúdo do primeiro
 seguido do conteúdo do segundo).
@@ -19,7 +19,7 @@ int main()
 	scanf(" %300[^\n]", file_one);
 	getExt(file_one, ext_one);
 
-	printf("Nome do arquivo 02:\n>>> ");
+	printf("\nNome do arquivo 02:\n>>> ");
 	scanf(" %300[^\n]", file_two);
 	getExt(file_two, ext_two);
 
@@ -29,23 +29,36 @@ int main()
 		return 1;
 	}else{
 		FILE * pone, * ptwo;
-		char file_three[13] = "new";
+		char file_three[21] = "new";
 		strcat(file_three, ext_one);/*Cria o nome do novo arquivo e a extensão dele*/
+
 		if(!(strcmp(ext_one, ".bin"))){
+			if((pone = fopen(file_one, "rb")) == NULL){
+                printf("Arquivo '%s' vazio ou inexistente!\nImpossivel fazer a gravacao!\n", file_one);
+                return 2;
+			}
 			ptwo = fopen(file_three, "wb");
-			pone = fopen(file_one, "rb");
 			cpyFileBin(pone, ptwo);
 			fclose(pone);
-			pone = fopen(file_two, "rb");
+			if((pone = fopen(file_two, "rb")) == NULL){
+                printf("Arquivo '%s' vazio ou inexistente!\nImpossivel prosseguir com a gravacao!\n", file_two);
+                return 2;
+			}
 			cpyFileBin(pone, ptwo);
 			fclose(pone);
 			fclose(ptwo);
 		}else{
-            		ptwo = fopen(file_three,"w");
-            		pone = fopen(file_one, "r");
+            if((pone = fopen(file_one, "r")) == NULL){
+                printf("Arquivo '%s' vazio ou inexistente!\nImpossivel fazer a gravacao!\n", file_one);
+                return 2;
+            }
+            ptwo = fopen(file_three,"w");
 			cpyFile(pone, ptwo);
 			fclose(pone);
-			pone = fopen(file_two, "r");
+			if((pone = fopen(file_two, "r")) == NULL){
+                printf("Arquivo '%s' vazio ou inexistente!\nImpossivel prosseguir com a gravacao!\n", file_two);
+                return 2;
+			}
 			cpyFile(pone, ptwo);
 			fclose(pone);
 			fclose(ptwo);
@@ -69,23 +82,24 @@ void cpyFile(FILE * pone, FILE * ptwo)
 	/*Armazena o conteúdo do arquivo passado em uma string e grava essa string no novo arquivo*/
 	char ch[1000000];
 	while(!feof(pone)){
-        	fgets(ch, 1000000, pone);
-        	fputs(ch, ptwo);
+        fgets(ch, sizeof(ch), pone);
+        fputs(ch, ptwo);
 	}
 }
 
 void cpyFileBin(FILE * pone, FILE * ptwo)
 {
-    	/*Calcula o tamanho do arquivo binário em bytes*/
+    /*Calcula o tamanho do arquivo binário em bytes*/
 	long len;
 	fseek(pone, 0, SEEK_END);
 	len = ftell(pone);
-	int v[len/4];
-	fread(v, sizeof(int), len / 4, pone);
-	fwrite(v, sizeof(int), len / 4, ptwo);
+	int v[len/2];
+	/*Grava o conteúdo dos arquivos para o novo arquivo*/
+	fread(v, sizeof(int), len / 2, pone);
+	fwrite(v, sizeof(int), len / 2, ptwo);
 }
 /*
-Nome: Ricardo de Freitas Olveira. 
+Nome: Ricardo de Freitas Olveira.
 Curso: Ciência da Computação.
 R.A.: 5934078.
 Exe.: 04.
