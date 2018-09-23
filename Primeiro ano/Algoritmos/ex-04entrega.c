b907693fc09706245f373e0dbdb9110ff779da19
Ricardo Oliveira <40704273+ricardo-fo@users.noreply.github.com>
2018-09-23 16:00:22 -0300

    ex-04-trabalho[finalizar]-fix-binary

--- Primeiro ano/Algoritmos/ex-04entrega.c
+++ Primeiro ano/Algoritmos/ex-04entrega.c
@@ -1,4 +1,4 @@
-/*
+/* 
 Faça um programa que receba dois arquivos do usuário, e crie um terceiro
 arquivo com o conteúdo dos dois primeiros juntos (o conteúdo do primeiro
 seguido do conteúdo do segundo).
