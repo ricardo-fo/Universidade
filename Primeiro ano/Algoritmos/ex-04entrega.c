b44311ce9f386f45444fbc4fd4488c8fd6d79b20
Ricardo Oliveira <40704273+ricardo-fo@users.noreply.github.com>
2018-09-27 16:29:21 -0300

    ex-04-trabalho[finalizado]
    Retorno da função cpyFileBin adicionado;
    Correções na gravação de arquivos binários;
    Remoção da captura do tamanho, em bytes, de arquivos.

--- Primeiro ano/Algoritmos/ex-04entrega.c
+++ Primeiro ano/Algoritmos/ex-04entrega.c
@@ -6,10 +6,11 @@ seguido do conteúdo do segundo).
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#define MAX 100000
 
 void getExt(const char *, char *);/*Pega o formato da extensão do arquivo*/
 void cpyFile(FILE *, FILE *);/*Faz a transferencia do conteúdo do arquivo para o novo arquivo*/
-void cpyFileBin(FILE *, FILE *);/*Faz a transferencia do conteúdo de um arquivo binário para o novo arquivo binário*/
+int cpyFileBin(FILE *, FILE *, int *);/*Faz a transferencia do conteúdo de um arquivo binário para o novo arquivo binário. Retorna a quantidade de números adicionados ao arquivo*/
 
 int main()
 {
@@ -29,34 +30,36 @@ int main()
 		return 1;
 	}else{
 		FILE * pone, * ptwo;
+		int storage[MAX] = {0}, lenght;
 		char file_three[21] = "new";
 		strcat(file_three, ext_one);/*Cria o nome do novo arquivo e a extensão dele*/
 
 		if(!(strcmp(ext_one, ".bin"))){
 			if((pone = fopen(file_one, "rb")) == NULL){
-                printf("Arquivo '%s' vazio ou inexistente!\nImpossivel fazer a gravacao!\n", file_one);
+                printf("\nArquivo '%s' vazio ou inexistente!\nImpossivel fazer a gravacao!\n", file_one);
                 return 2;
 			}
 			ptwo = fopen(file_three, "wb");
-			cpyFileBin(pone, ptwo);
+			lenght = cpyFileBin(pone, ptwo, storage);
 			fclose(pone);
 			if((pone = fopen(file_two, "rb")) == NULL){
-                printf("Arquivo '%s' vazio ou inexistente!\nImpossivel prosseguir com a gravacao!\n", file_two);
+                printf("\nArquivo '%s' vazio ou inexistente!\nImpossivel prosseguir com a gravacao!\n", file_two);
                 return 2;
 			}
-			cpyFileBin(pone, ptwo);
+			lenght = cpyFileBin(pone, ptwo, storage);
+            fwrite(storage, sizeof(int), lenght, ptwo);/*gravação no arquivo*/
 			fclose(pone);
 			fclose(ptwo);
 		}else{
             if((pone = fopen(file_one, "r")) == NULL){
-                printf("Arquivo '%s' vazio ou inexistente!\nImpossivel fazer a gravacao!\n", file_one);
+                printf("\nArquivo '%s' vazio ou inexistente!\nImpossivel fazer a gravacao!\n", file_one);
                 return 2;
             }
             ptwo = fopen(file_three,"w");
 			cpyFile(pone, ptwo);
 			fclose(pone);
 			if((pone = fopen(file_two, "r")) == NULL){
-                printf("Arquivo '%s' vazio ou inexistente!\nImpossivel prosseguir com a gravacao!\n", file_two);
+                printf("\nArquivo '%s' vazio ou inexistente!\nImpossivel prosseguir com a gravacao!\n", file_two);
                 return 2;
 			}
 			cpyFile(pone, ptwo);
@@ -79,24 +82,23 @@ void getExt(const char * file, char * ext)
 
 void cpyFile(FILE * pone, FILE * ptwo)
 {
-	/*Armazena o conteúdo do arquivo passado em uma string e grava essa string no novo arquivo*/
-	char ch[1000000];
+	/*Armazena o conteúdo do arquivo passado numa string e grava essa string no novo arquivo*/
+	char ch[MAX];
 	while(!feof(pone)){
         fgets(ch, sizeof(ch), pone);
         fputs(ch, ptwo);
 	}
 }
 
-void cpyFileBin(FILE * pone, FILE * ptwo)
+int cpyFileBin(FILE * pone, FILE * ptwo, int * arm)
 {
-    /*Calcula o tamanho do arquivo binário em bytes*/
-	long len;
-	fseek(pone, 0, SEEK_END);
-	len = ftell(pone);
-	int v[len/2];
+    static int i = 0;//i representa a quantidade de números adicionados no arquivo
 	/*Grava o conteúdo dos arquivos para o novo arquivo*/
-	fread(v, sizeof(int), len / 2, pone);
-	fwrite(v, sizeof(int), len / 2, ptwo);
+	fread(&arm[i], sizeof(int), MAX, pone);
+	while(arm[i] != 0){
+        i++;
+    }
+    return i;
 }
 /*
 Nome: Ricardo de Freitas Olveira.
