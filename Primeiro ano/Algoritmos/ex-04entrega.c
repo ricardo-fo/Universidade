4883e871ad77cf30a80bec9658ec3bdc975cdfde
Ricardo Oliveira <40704273+ricardo-fo@users.noreply.github.com>
2018-09-27 16:39:59 -0300

    ex-04-trabalho[finalizado]

--- Primeiro ano/Algoritmos/ex-04entrega.c
+++ Primeiro ano/Algoritmos/ex-04entrega.c
@@ -2,7 +2,7 @@
 Faça um programa que receba dois arquivos do usuário, e crie um terceiro
 arquivo com o conteúdo dos dois primeiros juntos (o conteúdo do primeiro
 seguido do conteúdo do segundo).
-*/
+*/ 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
