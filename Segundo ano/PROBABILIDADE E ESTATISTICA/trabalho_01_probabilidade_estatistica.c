a534d77f09718c5e853441e1a0bd0dfb901db8cc
Ricardo Freitas <ricardo-cadastros@hotmail.com>
2019-05-30 13:45:38 -0300

    Inputs e parse dos delimitadores

--- Segundo ano/PROBABILIDADE E ESTATISTICA/trabalho_01_probabilidade_estatistica.c
+++ Segundo ano/PROBABILIDADE E ESTATISTICA/trabalho_01_probabilidade_estatistica.c
@@ -8,9 +8,11 @@
  * Discplina: Probabilidade e Estatística - I.
  * Universidade  Católica de Santos - Maio de 2019.
 */
-#include <stdio.h> // utilizacao: inptus e outputs.
-#include <ctype.h> // utilizacao: isdigiy();
-#include <stdlib.h> // fprintf(); fscanf();
+#include <stdio.h> // uso: inptus e outputs.
+//#include <ctype.h> // uso: isdigit();
+#include <stdlib.h> // uso: fprintf(); fscanf();
+#include <string.h> // uso: strcat(); strcmp(); strrchr(); strtok();
+#include <stdbool.h> // uso: true e false;
 
 // Esta funcao deve identificar o caractere que separa a colunas do arquivo passado.
 // Retorno: caractere de separacao usado.
@@ -30,7 +32,10 @@ int main()
     printf("\n------------------------------------------------------------------------------------------------------\n");
 
     // Variáveis que serão utilizadas no menu
-    char opcao;
+    char opcao, buffer;
+    FILE * p_arquivo; // ponteiro para abrir o arquivo.
+    int i, qntdd_dados;
+    double col_01, col_02;
 
     // Menu principal.
     do{
@@ -42,7 +47,7 @@ int main()
         scanf(" %c", &opcao);
         while((getchar()) != '\n'); // Eliminação de digitos em excesso (limpar o buffer).
 
-        char nome_arquivo[101];
+        char nome_arquivo[101], novo_arquivo[101], separador = ',', * extensao;
 
         switch(opcao){
             case '1':
@@ -50,12 +55,10 @@ int main()
                 printf("\n......................................................................................................\n");
                 printf("\nInforme o nome do arquivo junto a sua extensao.");
                 printf("\nExemplo: meu_arquivo.txt\n");
-                printf("\nNome do arquivo:");
-                scanf(" %100[^\n]s", nome_arquivo);
-                while((getchar() != '\n'));
+                printf("\nNome do arquivo: ");
+                scanf(" %100[^\n]", nome_arquivo);
+                while((buffer = getchar() != '\n'));
 
-                // printf("\nSeu arquivo: %s\n", nome_arquivo); // Apenas para testar se o input foi realizado com sucesso.
-                FILE * p_arquivo; // ponteiro para abrir o arquivo.
                 if((p_arquivo = fopen(nome_arquivo, "r")) == NULL){
                     // Validação do arquivo passado.
                     // Utilização de padrão visual: asteriscos(*) para mensagens de erros.
@@ -63,18 +66,94 @@ int main()
                     fprintf(stderr, "\nErro: Arquivo '%s' nao foi encontrado ou nao pode ser aberto.", nome_arquivo);
                     printf("\n******************************************************************************************************");
                     fclose(p_arquivo);
-                } else {
-                    /*
-                        --> CONTINUAR DAQUI <--
-                    */
-                    printf("\ntudo ok\n");
+                    break;
                 }
+                int j = 0, size;
+                i = 0;
+                char linha[22], * token, separador_str[2];
+
+                printf("\nSeparador: ");
+                scanf(" %1s", separador_str);
+                while((buffer = getchar()) != '\n');
+
+                double vet_a[10] = {[0] = 0}, vet_b[10] = {[0] = 0};
+
+                while(fgets(linha, 22, p_arquivo) != NULL){
+                    //printf("\n%s", linha);
+                    token = strtok(linha, separador_str);
+                    if(token == NULL){
+                        fprintf(stderr, "\nERRO: '%s' usa mais de um tipo de delimitador ou o delimitador passado nao corresponde aos do arquivo.\n", nome_arquivo);
+                        return -1;
+                    }
+
+                    vet_a[i++] = atof(token);
+                    printf("\nvet_a[%d] = %lf", i-1, vet_a[i-1]);
+
+                    token = strtok(NULL, separador_str);
+                    if(token == NULL){
+                        fprintf(stderr, "\nERRO: '%s' usa mais de um tipo de delimitador ou o delimitador passado nao corresponde aos do arquivo.\n", nome_arquivo);
+                        return -1;
+                    }
+                    vet_b[j++] = atof(token);
+                    printf("\nvet_b[%d] = %lf", j-1, vet_b[j-1]);
+                }
+
+                /*for(i = 0; i < 10; i++){
+                    printf("\n%lf", vet_a[i]);
+                }
+                printf("\n");
+                for(i = 0; i < 10; i++){
+                    printf("\n%lf", vet_b[i]);
+                }*/
+
                 printf("\n......................................................................................................\n");
                 break;
             case '2':
                 // Utilização de padrão visual: pontos(.) para divisão opções do menu.
                 printf("\n......................................................................................................\n");
-                printf("\nOpcao 2:");
+                printf("\nNome do novo arquivo: ");
+                scanf("%100[^\n]", novo_arquivo);
+
+                extensao = strrchr(novo_arquivo, '.');
+                if(extensao == NULL){
+                    strcat(novo_arquivo, ".txt");
+                } else {
+                    if(strcmp(extensao, ".txt") != 0){
+                        strcat(novo_arquivo, ".txt");
+                    }
+                }
+
+                if((p_arquivo = fopen(novo_arquivo, "w")) == NULL){
+                    // Validação do arquivo passado.
+                    // Utilização de padrão visual: asteriscos(*) para mensagens de erros.
+                    printf("\n******************************************************************************************************");
+                    fprintf(stderr, "\nErro: Arquivo '%s' nao foi encontrado ou nao pode ser aberto.", novo_arquivo);
+                    printf("\n******************************************************************************************************");
+                    fclose(p_arquivo);
+                    break;
+                }
+
+                printf("\nSeu arquivo deve possuir duas colunas de dados numericos.");
+                printf("\nQuantidade de dados a ser inserido: ");
+                while((scanf("%d", &qntdd_dados) != 1) || qntdd_dados < 0){
+                    while((buffer = getchar()) != '\n');
+                    printf("\nValor invalido.\n");
+                    printf("\nQuantidade de dados a ser inserido: ");
+                }
+
+                printf("\nSimbolo a ser usado: ");
+                scanf(" %c", &separador);
+                while((buffer = getchar()) != '\n');
+
+                printf("\nInsercao de dados. Utilize espaco para cada coluna. Exemplo:\n2.8 3.7\n4.6 5.5\n1.9 7.3\n. . .\n");
+                for(i = 0; i < qntdd_dados/2; i++){
+                    printf(">>> ");
+                    scanf("%lf %lf", &col_01, &col_02);
+                    fprintf(p_arquivo, "%lf%c%lf%c", col_01, separador, col_02, '\n');
+                }
+                fclose(p_arquivo);
+
+                printf("\nSeu arquivo foi criado com sucesso.\n");
                 printf("\n......................................................................................................\n");
             case '0':
                 break;
